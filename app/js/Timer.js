c3310b7e0012b0cd2d10595fce0a941c14d8963a
zolbayars <zolube@gmail.com>
2018-10-26 15:06:25 +0800

    Added main countdown timer

--- app/js/Timer.js
+++ app/js/Timer.js
@@ -1,16 +1,26 @@
 'use strict';
 
-function modMinute(minute, seconds){
+const e = React.createElement;
+
+function modMinute(minute){
+  console.log("modMinute called: " + minute);
   if(minute > 10){
     return minute - 1;
+  }else if (minute === '00'){
+    return 25
   }else{
     return 0 + "" + (minute - 1);
   }
 }
 
 function modSeconds(seconds){
+  console.log("modSeconds called: " + seconds);
   if(seconds > 10){
     return seconds - 1;
+  }else if (seconds === 0){
+    return '00'
+  }else if (seconds === '00'){
+    return 59
   }else{
     return 0 + "" + (seconds - 1);
   }
@@ -21,17 +31,37 @@ class Timer extends React.Component {
     super(props);
     this.state = {
       minutes: '25',
-      seconds: '00'
+      seconds: '00',
+      actionBtnName: 'START',
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
   tick() {
 
-    if()
+    if(this.state.seconds == '00' || this.state.seconds == 0){
+      this.setState(state => ({
+        minutes: modMinute(state.minutes),
+        seconds: modSeconds(state.seconds)
+      }));
+    }else{
+      this.setState(state => ({
+        seconds: modSeconds(state.seconds)
+      }));
+    }
+
+  }
+
+  handleClick() {
+    if(this.state.actionBtnName == 'START'){
+      this.interval = setInterval(() => this.tick(), 1000);
+    }else{
+      clearInterval(this.interval);
+    }
 
     this.setState(state => ({
-      minutes: modMinute(state.minutes);
-      seconds: modSeconds(state.seconds);
+      actionBtnName: state.actionBtnName == 'START' ? 'STOP' : 'START'
     }));
   }
 
@@ -44,40 +74,10 @@ class Timer extends React.Component {
   }
 
   render() {
-    return React.createElement(
-      "div",
-      null,
-
-      this.state.minutes + ":" + this.state.seconds
-    );
+    return e("div", null, this.state.minutes + ":" + this.state.seconds,
+              e("a",{ id: "start-btn", onClick: this.handleClick }, this.state.actionBtnName ));
   }
 }
 
 const domContainer = document.querySelector('#timer-container');
-ReactDOM.render(React.createElement(Timer, null), domContainer);
-
-//
-//
-// const e = React.createElement;
-//
-// class LikeButton extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { liked: false };
-//   }
-//
-//   render() {
-//     if (this.state.liked) {
-//       return 'You liked this.';
-//     }
-//
-//     return e(
-//       'button',
-//       { onClick: () => this.setState({ liked: true }) },
-//       'Like'
-//     );
-//   }
-// }
-//
-// const domContainer = document.querySelector('#timer-container');
-// ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(Timer, null), domContainer);
